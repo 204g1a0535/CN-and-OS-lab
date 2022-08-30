f1c28a36bc2a3e5cc56dafa657170c8bdc39b392
204g1a0535 <111737869+204g1a0535@users.noreply.github.com>
2022-08-30 11:21:29 +0530

    Create SAMPLE.C
    S . Guru Sai Charan created SAMPLE.C

---
+++ SAMPLE.C
@@ -0,0 +1,6 @@
+/* Name Of the Candidate: s.  Guru Sai Charan
+   Roll No: 204G1A0535
+   Title of the Experiment: CPU Scheduling Using FCFS Algorithm
+   Date of Creation: 30-08-2022
+   Date of Execution: 30-08-2022
+*/
